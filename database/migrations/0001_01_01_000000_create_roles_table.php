267ae24e4cc5cc528077fa980fb72a886fe2dbd1
Gabigabe <mhammed.gabigabe@gmail.com>
2026-04-08 16:17:26 +0100

    refactor: réorganisation des fichiers de migration pour respecter les dépendances entre tables

--- database/migrations/0001_01_01_000000_create_roles_table.php
+++ database/migrations/0001_01_01_000000_create_roles_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('titre')->unique;
+            $table->string('titre')->unique();
             $table->timestamps();
         });
     }
