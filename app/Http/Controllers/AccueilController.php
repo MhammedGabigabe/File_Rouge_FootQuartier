ba28001db2aea0e98e0bb64ef73ca632e858d895
Gabigabe <mhammed.gabigabe@gmail.com>
2026-04-13 12:15:50 +0100

    feat: ajout de controller AccueilController

---
+++ app/Http/Controllers/AccueilController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Terrain;
+use App\Models\Annonce;
+
+class AccueilController extends Controller
+{
+    public function index()
+    {
+        $terrains = Terrain::where('statut', 'actif')
+            ->latest()
+            ->take(3)
+            ->get();
+
+        $annonces = Annonce::where('statut', 'ouverte')
+            ->with('reservation.terrain', 'organisateur')
+            ->latest()
+            ->take(3)
+            ->get();
+
+        return view('accueil', compact('terrains', 'annonces'));
+    }
+}
