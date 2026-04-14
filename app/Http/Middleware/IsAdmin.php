03d8397258b39bec993a465c0dae9ff48b7f62b5
Gabigabe <mhammed.gabigabe@gmail.com>
2026-04-14 11:15:14 +0100

    fix: modifier la methode

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,14 +4,15 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $user = Auth::user();
-        if (!$user || !Auth::user()->isAdmin()) {
+        if (!$user || !$user->isAdmin()) {
             abort(403, 'Accès réservé aux administrateurs.');
         }
         return $next($request);
