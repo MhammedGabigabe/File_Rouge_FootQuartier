7b93b682abdd542c4ee783c0442737f8fbd32e71
Gabigabe <mhammed.gabigabe@gmail.com>
2026-04-14 11:15:33 +0100

    fix: modifier la methode

--- app/Http/Middleware/IsModerateur.php
+++ app/Http/Middleware/IsModerateur.php
@@ -4,14 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
+
 
 class IsModerateur
 {
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $user = Auth::user();
-        if (!$user || !Auth::user()->isModerateur()) {
+        if (!$user || !$user->isModerateur()) {
             abort(403, 'Accès réservé aux modérateurs.');
         }
 
