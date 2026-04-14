85b4e040b84f6bba78e56041c8469627307b7223
Gabigabe <mhammed.gabigabe@gmail.com>
2026-04-14 12:58:51 +0100

    refactore: amelioration de la vue

--- resources/views/joueur_dashboard.blade.php
+++ resources/views/joueur_dashboard.blade.php
@@ -73,14 +73,6 @@
                     Tableau de bord
                 </a>
 
-                <a href="{{ route('moderator.dashboard') }}" class="nav-link w-full">
-                    <svg class="w-4 h-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                    </svg>
-                    Espace Modérateur
-                </a>
-
                 <a href="{{ route('terrains') }}" class="nav-link {{ request()->routeIs('terrains') ? 'active' : '' }}">
                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -148,6 +140,29 @@
                         </span>
                     @endif
                 </a>
+                @php
+                    $roleMod = Auth::user()->isModerateur();
+                    $roleAdmin = Auth::user()->isAdmin();
+                @endphp
+                @if ($roleMod)
+                    <a href="{{ route('moderator.dashboard') }}" class="nav-link w-full">
+                        <svg class="w-4 h-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                        Espace Modérateur
+                    </a>
+                @endif
+
+                @if ($roleAdmin)
+                    <a href="{{ route('admin.dashboard') }}" class="nav-link w-full">
+                        <svg class="w-4 h-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                        Espace Admin
+                    </a>
+                @endif
 
             </nav>
         </div>
